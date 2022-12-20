bb4a398c74ed1d2981f14ee5c73195599575f5f2
Tuombe <jtuombe@gmail.com>
2022-12-20 09:46:10 +0200

    practice1

---
+++ titi/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/amazona">amazona</a>
+      </header>
+      <main>frontend</main>
+    </div>
+  );
+}
+
+export default App;
