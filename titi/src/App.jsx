1efac7c2e21961cca4d46463a01123dba016ac29
Tuombe <jtuombe@gmail.com>
2022-12-20 11:10:00 +0200

    changes

--- titi/src/App.jsx
+++ titi/src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/amazona">amazona</a>
-      </header>
-      <main>frontend</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/amazona">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
